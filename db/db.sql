2d8883da78c8b0a8c46331a4e0f0c62d41fbd2e5
peixingzhe <peixingzhe@163.com>
2021-04-24 18:59:44 +0800

    db

---
+++ db/db.sql
@@ -0,0 +1,148 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 8.0.16 : Database - user
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`user` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `user`;
+
+/*Table structure for table `bzorder` */
+
+DROP TABLE IF EXISTS `bzorder`;
+
+CREATE TABLE `bzorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL,
+  `rpstName` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `productName` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `productNum` int(11) DEFAULT NULL,
+  `userName` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `des` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `bzorder` */
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `product` */
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`) values (1,'manager');
+
+/*Table structure for table `rpst` */
+
+DROP TABLE IF EXISTS `rpst`;
+
+CREATE TABLE `rpst` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `place` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `rpst` */
+
+/*Table structure for table `rpst_product` */
+
+DROP TABLE IF EXISTS `rpst_product`;
+
+CREATE TABLE `rpst_product` (
+  `rpstId` int(11) DEFAULT NULL,
+  `productId` int(11) DEFAULT NULL,
+  `productNum` int(11) DEFAULT NULL,
+  KEY `key_rpstId_2` (`rpstId`),
+  KEY `key_productId_2` (`productId`),
+  CONSTRAINT `key_productId_2` FOREIGN KEY (`productId`) REFERENCES `product` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `key_rpstId_2` FOREIGN KEY (`rpstId`) REFERENCES `rpst` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `rpst_product` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `phone` varchar(11) COLLATE utf8_bin DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `sex` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`phone`,`level`,`email`,`sex`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3','12345612341',1,'admin@qq.com',1);
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `userId` int(11) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  KEY `userId` (`userId`),
+  KEY `key_roleId_3` (`roleId`),
+  CONSTRAINT `key_roleId_3` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `key_userId_3` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`userId`,`roleId`) values (1,1);
+
+/*Table structure for table `user_rpst` */
+
+DROP TABLE IF EXISTS `user_rpst`;
+
+CREATE TABLE `user_rpst` (
+  `userId` int(11) DEFAULT NULL,
+  `rpstId` int(11) DEFAULT NULL,
+  KEY `key_user_1` (`userId`),
+  KEY `key_rpst_2` (`rpstId`),
+  CONSTRAINT `key_rpst_2` FOREIGN KEY (`rpstId`) REFERENCES `rpst` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `key_user_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user_rpst` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
